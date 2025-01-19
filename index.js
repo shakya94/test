4b56c528d43a4cee4dae1b18338c3f2d961110f0
shakya94 <57297686+shakya94@users.noreply.github.com>
2025-01-19 13:18:07 +0530

    corsd

--- index.js
+++ index.js
@@ -22,7 +22,31 @@ mongoose.connect('mongodb+srv://harshitshakya94:Kanpur94%40@studentopedia.ufyih.
     console.error('Error connecting to MongoDB:', err);
 });
   
-  
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: [
+      "https://studentopedia.in",
+      "http://localhost:3000",
+    ],
+  })
+);
+
+// Handle preflight requests (OPTIONS)
+app.options("*", (req, res) => {
+  const allowedOrigins = [
+    "https://studentopedia.in",
+    "http://localhost:3000",
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.sendStatus(200);
+});
+
 
 app.use('/api',aboutroutes);
 app.use('/api',contactroutes);
