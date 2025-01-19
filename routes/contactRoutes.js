40f3262ba2bcd457840d49ba96875eb5ded2a333
shakya94 <57297686+shakya94@users.noreply.github.com>
2025-01-19 13:41:36 +0530

    contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,7 +6,7 @@ const contact = require("../models/contacts");
 router.post('/contact',cors,async (req,res)=>{
     const contactdata = new contact(req.body);
     contactdata.save()
-    .then(()=> res.status(500).json({message :"Sucess"}))
+    .then(()=> res.status(200).json({message :"Sucess"}))
     .catch(err =>
     {
         res.status(500).json({message :err})
